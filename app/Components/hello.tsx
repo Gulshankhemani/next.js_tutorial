cb5553cb2946c84f787adca23c28b0563af91d5f
Gulshan khemani <gulshankhemani2@gmail.com>
2025-08-02 23:54:24 +0530

    second commit

--- app/Components/hello.tsx
+++ app/Components/hello.tsx
@@ -1,6 +1,6 @@
 "use client";
 
 export default function Hello() {
+    console.log('i am a client component');
     return <h1>Hello World</h1>;
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { log } from "console";
+import Hello from "./Components/hello";
 
 
 export default function Home() {
+  console.log('what am i doing here  --- server/client component');
   return (
-    <h1 className="text-3xl justify-center align-center">Welcome to Next.js tutorial</h1>
+    <>
+      <h1 className="text-3xl justify-center align-center">Welcome to Next.js tutorial</h1>
+      <Hello/>
+    </>
   );
 }
