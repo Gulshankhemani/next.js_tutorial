5f3dd2a558c3a5957d0c675451538e2780b93e17
Gulshan khemani <gulshankhemani2@gmail.com>
2025-08-03 15:49:14 +0530

    third commit

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import Link from "next/link";
 
-export const page = () => {
+export default function UsersPage() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <Link href="/dashboard/users/1">User 1</Link>
+      <Link href="/dashboard/users/2">User 2</Link>
+      <Link href="/dashboard/users/3">User 3</Link> 
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { log } from "console";
 import Hello from "./Components/hello";
 
 
@@ -6,7 +5,6 @@ export default function Home() {
   console.log('what am i doing here  --- server/client component');
   return (
     <>
-      <h1 className="text-3xl justify-center align-center">Welcome to Next.js tutorial</h1>
       <Hello/>
     </>
   );
